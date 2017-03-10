85c9ee00fea93dba49c74a420b01fd29f78f9053
SAbahri <SAbahri@ibs.ru>
2017-03-10 12:56:24 +0500

    add travis ci and github key

--- src/App.js
+++ src/App.js
@@ -3,19 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = {
+        repos: [],
+    }
+
+    componentDidMount() {
+        fetch('/github')
+            .then(response => response.json())
+            .then((data) => {
+                const repos = data.map((repo) =>
+                    <p key={repo.id}>{repo.name}</p>
+                );
+                this.setState({ repos })
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <h3>App Creator's Repos:</h3>
+                {this.state.repos}
+            </div>
+        );
+    }
 }
 
 export default App;
